e2323bc71250a7c1e6a2e008f925458d793d3b06
White Wolf <Artem.ilin.spb@yandex.ru>
2025-10-25 22:34:22 +0300

    base

---
+++ Pifagor.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+#include <cmath>
+
+using namespace std;
+
+bool isPythagoreanTriple(int a, int b, int c) {
+    // Проверяем все комбинации a² + b² = c²
+    long long a2 = (long long)a * a;
+    long long b2 = (long long)b * b;
+    long long c2 = (long long)c * c;
+
+    return (a2 + b2 == c2) || (a2 + c2 == b2) || (b2 + c2 == a2);
+}
